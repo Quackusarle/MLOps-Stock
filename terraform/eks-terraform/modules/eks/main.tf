c24dd32afb6cc1a60132a45a14df0eacd723fbeb
Quackusarle <tvhoangg05@gmail.com>
2026-06-19 11:38:38 +0700

    fix: add setup python/aws cli in workflow and adjust EKS security group rules

--- terraform/eks-terraform/modules/eks/main.tf
+++ terraform/eks-terraform/modules/eks/main.tf
@@ -35,11 +35,19 @@ module "eks" {
       description                   = "Allow access from control plane to webhook port of ALB controller"
     }
     ingress_vpc_all = {
-      description = "Allow all traffic from VPC (Pod to Pod cross-node)"
+      description = "Allow all traffic from VPC"
       protocol    = "-1"
       from_port   = 0
       to_port     = 0
       type        = "ingress"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+    egress_vpc_all = {
+      description = "Allow all outbound traffic to VPC"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "egress"
       cidr_blocks = ["10.0.0.0/16"]
     }
   }
